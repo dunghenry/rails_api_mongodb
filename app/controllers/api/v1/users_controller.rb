f685e0bc93f80a93b136c9616f66a1e9cd4ee7d6
dunghenry <trandungksnb6930082001@gmail.com>
2022-08-11 21:48:43 +0700

    crud and validate, hash pass

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,56 @@
+class Api::V1::UsersController < ApplicationController
+    before_action :getUser, only: [:showUser, :updateUser, :deleteUser]
+    def getUsers
+        @users = User.all
+        if @users
+            render json: @users, status: :ok
+        else
+            render json: {msg: "Users is empty"}, status: :unprocessable_entity
+        end
+    end
+    def showUser
+        if @user
+            render json: @user, status: :ok
+        else
+            render json:{msg: "User not found"},status: :unprocessable_entity
+        end
+    end
+    def addUser
+        user = User.new(userparams)
+        # user = User.new(username: params[:username], email: params[:email], password_digest: params[:password])
+        if user.save()
+            render json:user, status: :ok
+        else 
+            render json: {msg: "User not added", error: user.errors}, status: :unprocessable_entity
+        end
+    end
+    def updateUser
+        if @user
+            if @user.update(userparams)
+                render json: @user, status: :ok
+            else
+                render json:{msg: "Updated user failed", error: @user.errors},status: :unprocessable_entity
+            end
+        else
+            render json:{msg: "User not found"},status: :unprocessable_entity
+        end
+    end
+    def deleteUser
+        if @user
+            if @user.destroy()
+                render json:{msg: "Deleted user successfully"},status: :ok
+            else
+                render json:{msg: "Deleted user failed"},status: :unprocessable_entity
+            end
+        else
+            render json:{msg: "User not found"},status: :unprocessable_entity
+        end
+    end
+    private
+    def userparams
+        params.permit(:username, :email, :password)
+    end
+    def getUser
+        @user = User.find(params[:id])
+    end
+end
